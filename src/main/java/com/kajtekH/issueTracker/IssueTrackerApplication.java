e1da74b2c697625e349d481677b09eec323fe5a9
KajtekH <kajhulboj@wp.pl>
2024-03-06 22:55:12 +0100

    Init

---
+++ src/main/java/com/kajtekH/issueTracker/IssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kajtekH.issueTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kajtekH/issueTracker/model/User.java
@@ -0,0 +1,46 @@
+package com.kajtekH.issueTracker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)		
+	private int id;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "username")
+	private String username;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+	
+	
+}
